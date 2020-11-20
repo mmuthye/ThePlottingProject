72df1452d2d0106647a34a98411da8f21d86e5dd
mmuthye <mandar_m@hotmail.com>
2020-11-20 20:19:14 +0530

    Final Version.

--- plot2.R
+++ plot2.R
@@ -1,11 +1,38 @@
-#Plot2
+#Plot 2
 
-# Add a reference to Plot1 file since the function to download and create filtered dataset is part of plot1
-# This way repeating the code to download and creating the filtered dataset is avoided 
-source("plot1.R")
+# Import the required libraries, if any
+library(dplyr)
+library(data.table)
 
-# Get the filtered dataset file by calling getDataset() method defined in Plot1.R file
-hpc_ds <- getDataset()
+# Define the hard values to be used in the code, 
+# includes - URL to the dataset, dataset filename and the dates to be filtered
+DatasetURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+DatasetZip <- "exdata_data_household_power_consumption.zip"
+DatasetFilename <- "household_power_consumption.txt" 
+FilterDate1 = "1/2/2007"
+FilterDate2 = "2/2/2007"
+
+
+# Check if the dataset already exists / downloaded. If not, then download the file 
+if (!file.exists(DatasetFilename)) {
+        download.file(DatasetURL, DatasetZip)
+        unzip(DatasetZip)
+} 
+
+# Load the Household Power Consumption (hpc) dataset
+hpc <- fread(file=DatasetFilename, header = TRUE, sep = "auto")
+
+# Define only the required subset of the dataset
+hpc_ds <- subset(hpc, Date == FilterDate1 | Date == FilterDate2)
+
+# Convert the Time and Date columns from character to Date and Time formats.
+# Note: Time needs to be convered into Date Time function
+hpc_ds <- mutate(hpc_ds, Time = strptime(paste(Date, Time, sep = " "), "%d/%m/%Y %H:%M:%S"))
+hpchpc_ds <- mutate(hpc_ds, Date = as.Date(Date, "%d/%m/%Y"))
+
+# Remove the unwanted variables that will no longer be required to free up memory
+rm(hpc)
+
 
 # Draw Plot 2
 plot(hpc_ds$Time, as.numeric(hpc_ds$Global_active_power), type = "l", ylab = "Global Active Power (kilowatts)", xlab = "")
